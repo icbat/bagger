8984647d2fd0ce28ba720e671c4effd8b019e33a
Evan Cobb <cobb.evan@gmail.com>
2016-08-27 14:17:50 -0500

    simplifying gulp based on work on square squared

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
+
+gulp.task('default', ['copy-bower'], function() {});
 
 var bower_components = [
-  "bower_components/phaser/build/phaser.min.js"
+    'bower_components/phaser/build/phaser.min.js'
 ];
 
-gulp.task('default',['copy-bower'], function() {});
-
 gulp.task('copy-bower', function() {
-  return gulp
-    .src(bower_components)
-    .pipe(gulp.dest('./www/scripts/vendor/'));
+    return gulp
+        .src(bower_components)
+        .pipe(gulp.dest('./www/js/vendor/'));
 });
