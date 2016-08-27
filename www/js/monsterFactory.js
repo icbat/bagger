33fffca3673596fa87bd58ff1a2089f4a3680a95
Evan Cobb <cobb.evan@gmail.com>
2016-08-27 16:25:56 -0500

    monsters now die after a time, and generate a new monster when it dies

--- www/js/monsterFactory.js
+++ www/js/monsterFactory.js
@@ -6,6 +6,22 @@ var monsterFactory = {
         monster.animations.add('walk');
         monster.animations.play('walk', 2, true);
 
+        monster.die = function() {
+            var tween = game.add.tween(this).to({
+                alpha: 0
+            }, Phaser.Timer.SECOND * 2, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(function() {
+                // TODO consider using kill and having this factory draw from an object pool
+                // If you do, remember that the game looks for onDestroy right now, that needs to be updated too
+                this.destroy();
+            }, this);
+        };
+
+        monster.events.onDestroy.add(function() {
+            this.createMonster(x, y);
+        }, this);
+        game.time.events.add(Phaser.Timer.SECOND * 3, monster.die, monster);
+
         return monster;
     }
 };
--- www/js/states/running.js
+++ www/js/states/running.js
@@ -2,11 +2,17 @@ var state_running = function(game) {
     return {
         create: function(game) {
             var upperScreenBottom = game.world.height / 4;
+
             var sky = game.add.sprite(0, 0, 'pixel');
             sky.scale.setTo(game.world.width, upperScreenBottom);
             sky.tint = Phaser.Color.hexToRGB(colors.blue);
 
-            // SUPER inefficient, probably. TODO : Profile this vs one giant texture
+            var bottom = game.add.sprite(0, upperScreenBottom * 3, 'pixel');
+            bottom.scale.setTo(game.world.width, upperScreenBottom);
+            bottom.tint = Phaser.Color.hexToRGB(colors.sand);
+
+            // SUPER inefficient, probably.
+            // TODO Profile this vs one giant texture
             // TODO also try sprite batch
             for (i = 0; i * 70 < game.world.width; ++i) {
                 game.add.sprite(i * 70, upperScreenBottom - 105, 'ground-middle');
@@ -18,11 +24,7 @@ var state_running = function(game) {
             player.scale.setTo(4, 4);
             player.anchor.setTo(0.5, 1);
 
-            var bottom = game.add.sprite(0, upperScreenBottom * 3, 'pixel');
-            bottom.scale.setTo(game.world.width, upperScreenBottom);
-            bottom.tint = Phaser.Color.hexToRGB(colors.sand);
-
-            monsterFactory.createMonster(game.world.width / 5 * 4, playerHeight);
+            var monster = monsterFactory.createMonster(game.world.width / 5 * 4, playerHeight);            
         }
     };
 };
