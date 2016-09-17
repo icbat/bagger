25bb1e22e6c6638953ff2bd7c96bbee602266dc5
Evan Cobb <cobb.evan@gmail.com>
2016-09-17 17:52:19 -0500

    pulling all of bag slot making into layout

--- www/js/layout.js
+++ www/js/layout.js
@@ -9,5 +9,20 @@ var layout = {
         var smallestSide = Math.min(height, width);
         var slotSize = (smallestSide - (5 * paddingBetweenSlots)) / 3;
         return slotSize;
+    },
+    fourthOfScreen: function(game) {
+        return game.world.height / 4;
+    },
+    addBagSlots: function(game) {
+        for (i = 0; i < 9; ++i) {
+            var slotSize = layout.findSlotSize(game.world.width, layout.fourthOfScreen(game) * 2, layout.bagSlotPadding);
+            var xPadding = layout.findPadding(game.world.width, slotSize, layout.bagSlotPadding);
+            var yPadding = layout.findPadding(layout.fourthOfScreen(game) * 2, slotSize, layout.bagSlotPadding);
+            var x = xPadding + (i % 3) * (slotSize + layout.bagSlotPadding);
+            var y = yPadding + (Math.floor(i / 3) * (slotSize + layout.bagSlotPadding));
+            var slot = game.add.sprite(x, layout.fourthOfScreen(game) + y, 'pixel');
+            slot.scale.setTo(slotSize, slotSize);
+            slot.tint = Phaser.Color.hexToRGB(colors.bagSlots);
+        }
     }
 };
--- www/js/states/running.js
+++ www/js/states/running.js
@@ -1,24 +1,22 @@
 var state_running = function(game) {
     return {
         create: function(game) {
-            var fourthOfScreen = game.world.height / 4;
-
             var sky = game.add.sprite(0, -100, 'pixel');
-            sky.scale.setTo(game.world.width, fourthOfScreen);
+            sky.scale.setTo(game.world.width, layout.fourthOfScreen(game));
             sky.tint = Phaser.Color.hexToRGB(colors.blue);
 
-            var bottom = game.add.sprite(0, fourthOfScreen * 3, 'pixel');
-            bottom.scale.setTo(game.world.width, fourthOfScreen + 100);
+            var bottom = game.add.sprite(0, layout.fourthOfScreen(game) * 3, 'pixel');
+            bottom.scale.setTo(game.world.width, layout.fourthOfScreen(game) + 100);
             bottom.tint = Phaser.Color.hexToRGB(colors.sand);
 
             // SUPER inefficient, probably.
             // TODO Profile this vs one giant texture
             // TODO also try sprite batch
             for (i = 0; i * 70 < game.world.width; ++i) {
-                game.add.sprite(i * 70, fourthOfScreen - 105, 'ground-middle');
+                game.add.sprite(i * 70, layout.fourthOfScreen(game) - 105, 'ground-middle');
             }
 
-            var playerHeight = fourthOfScreen - 64;
+            var playerHeight = layout.fourthOfScreen(game) - 64;
 
             var player = game.add.sprite(game.world.width * layout.playerXCoefficient, playerHeight, 'player');
             player.scale.setTo(4, 4);
@@ -26,16 +24,7 @@ var state_running = function(game) {
 
             var monster = monsterFactory.createMonster(game.world.width * layout.monsterXCoefficient, playerHeight);
 
-            for (i = 0; i < 9; ++i) {
-                var slotSize = layout.findSlotSize(game.world.width, fourthOfScreen * 2, layout.bagSlotPadding);
-                var xPadding = layout.findPadding(game.world.width, slotSize, layout.bagSlotPadding);
-                var yPadding = layout.findPadding(fourthOfScreen * 2, slotSize, layout.bagSlotPadding);
-                var x = xPadding + (i % 3) * (slotSize + layout.bagSlotPadding);
-                var y = yPadding + (Math.floor(i / 3) * (slotSize + layout.bagSlotPadding));
-                var slot = game.add.sprite(x, fourthOfScreen + y, 'pixel');
-                slot.scale.setTo(slotSize, slotSize);
-                slot.tint = Phaser.Color.hexToRGB(colors.bagSlots);
-            }
+            layout.addBagSlots(game);
         }
     };
 };
