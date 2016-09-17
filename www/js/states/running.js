8e875a9aea9fba33177e4a1659ffa5c200c8c5d8
Evan Cobb <cobb.evan@gmail.com>
2016-09-17 16:30:38 -0500

    renaming stuffs

--- www/js/states/running.js
+++ www/js/states/running.js
@@ -1,24 +1,24 @@
 var state_running = function(game) {
     return {
         create: function(game) {
-            var upperScreenBottom = game.world.height / 4;
+            var fourthOfScreen = game.world.height / 4;
 
             var sky = game.add.sprite(0, -100, 'pixel');
-            sky.scale.setTo(game.world.width, upperScreenBottom);
+            sky.scale.setTo(game.world.width, fourthOfScreen);
             sky.tint = Phaser.Color.hexToRGB(colors.blue);
 
-            var bottom = game.add.sprite(0, upperScreenBottom * 3, 'pixel');
-            bottom.scale.setTo(game.world.width, upperScreenBottom + 100);
+            var bottom = game.add.sprite(0, fourthOfScreen * 3, 'pixel');
+            bottom.scale.setTo(game.world.width, fourthOfScreen + 100);
             bottom.tint = Phaser.Color.hexToRGB(colors.sand);
 
             // SUPER inefficient, probably.
             // TODO Profile this vs one giant texture
             // TODO also try sprite batch
             for (i = 0; i * 70 < game.world.width; ++i) {
-                game.add.sprite(i * 70, upperScreenBottom - 105, 'ground-middle');
+                game.add.sprite(i * 70, fourthOfScreen - 105, 'ground-middle');
             }
 
-            var playerHeight = upperScreenBottom - 64;
+            var playerHeight = fourthOfScreen - 64;
 
             var player = game.add.sprite(game.world.width * constants.playerXCoefficient, playerHeight, 'player');
             player.scale.setTo(4, 4);
